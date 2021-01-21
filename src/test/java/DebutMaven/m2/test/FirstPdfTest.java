a0a7ff9d477e3288cdaf7efc5f08f499b6fe140f
Bertin Hugo <hugo.bertin.1@etudiant.univ-rennes1.fr>
2021-01-21 18:04:27 +0100

    test commit

---
+++ src/test/java/DebutMaven/m2/test/FirstPdfTest.java
@@ -0,0 +1,21 @@
+package DebutMaven.m2.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for FirstPdf.
+ */
+
+public class FirstPdfTest {
+	/**
+     * test Test
+     */
+    @Test
+    public void isCreated()
+    {
+        assertTrue( true );
+    }
+
+}
